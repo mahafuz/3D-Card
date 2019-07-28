41933a96d1c1f9f669d534d3c5eef39428afadc5
Mahafuz <m.mahfuz.me@gmail.com>
2019-07-28 11:56:39 +0600

    so so complete

---
+++ js/index.js
@@ -0,0 +1,28 @@
+(function($) {
+        
+    var card = $('.single-card');
+
+    card.map(function(index, item) {
+        
+        $(item).on("mousemove", function(e) {
+            var mX = e.clientX
+            var mY = e.clientY
+            var winHalfWidth = window.innerWidth/2
+            var winHalfHeight = window.innerHeight/2 
+            var xdeg = (mX - winHalfWidth)/winHalfWidth;
+            var ydeg = (mY - winHalfHeight)/winHalfHeight;
+            $(item).css('transform', `rotateX(${ydeg * 10}deg) rotateY(${xdeg * 10}deg)`);
+        });
+
+        window.ondevicemotion = function(event) {
+            var acX = event.accelerationIncludingGravity.x;
+            var acY = event.accelerationIncludingGravity.y;
+            var acZ = event.accelerationIncludingGravity.z;
+            let xdeg = acX/10;
+            let ydeg = acY/10;
+            $(item).css('transform', `rotateX(${ydeg * 20}deg) rotateY(${xdeg * 20}deg)`);
+        }
+
+    });
+
+})(jQuery);
